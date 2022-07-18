d42aec662c7ff41bdeaefba76dd01ccb840e3eb7
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-07-18 15:03:51 -0500

    add total cost to recipe index

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,4 +5,15 @@ class Recipe < ApplicationRecord
 
     has_many :recipe_ingredients
     has_many :ingredients, through: :recipe_ingredients
+
+    def total_cost
+        ingredient_ids = Recipe.joins(:recipe_ingredients).first.ingredient_ids
+
+        cost = 0 
+        ingredient_ids.each do |id| 
+            cost += Ingredient.find(id).cost
+        end
+
+        cost 
+    end
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -11,4 +11,23 @@ RSpec.describe Recipe, type: :model do
     it {should have_many :recipe_ingredients}
     it {should have_many(:ingredients).through(:recipe_ingredients)}
   end
+
+  describe 'instance methods' do 
+    it 'returns the cost of the recipe' do 
+      recipe1 = Recipe.create!(name: 'Spaghetti', complexity: 2, genre: 'Italian')
+
+      ingredient1 = Ingredient.create!(name: 'Pasta', cost: 2)
+      ingredient2 = Ingredient.create!(name: 'Eggplant', cost: 3)
+      ingredient3 = Ingredient.create!(name: 'Tomato sauce', cost: 5)
+      ingredient5 = Ingredient.create!(name: "Salt", cost: 4)
+
+
+      RecipeIngredient.create!(recipe: recipe1, ingredient: ingredient1)
+      RecipeIngredient.create!(recipe: recipe1, ingredient: ingredient2)
+      RecipeIngredient.create!(recipe: recipe1, ingredient: ingredient3)
+      RecipeIngredient.create!(recipe: recipe1, ingredient: ingredient5)
+
+      expect(recipe1.total_cost).to eq 14
+    end
+  end
 end
